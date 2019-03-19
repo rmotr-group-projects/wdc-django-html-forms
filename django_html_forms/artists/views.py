5c5b29947bd5c6e3cb58d58ac6e2133e12a2cd94
Jonathan Flowers <jmflowers134@gmail.com>
2019-03-18 22:16:02 -0400

    Update views.py

--- django_html_forms/artists/views.py
+++ django_html_forms/artists/views.py
@@ -38,8 +38,30 @@ def delete_song(request):
 
 
 def create_artist(request):
-    pass
+    artistic_name = request.POST['artistic_name']
+    first_name = request.POST['first_name']
+    last_name = request.POST['last_name']
+    picture_url = request.POST['picture_url']
+    popularity = request.POST['popularity']
+    genre = request.POST['genre']
 
+    Artist.objects.create(
+    artistic_name = artistic_name,
+    first_name = first_name,
+    last_name = last_name,
+    picture_url = picture_url,
+    popularity = int(popularity),
+    genre = genre,
+    )
+    
+    return redirect('artists')
+        
 
 def delete_artist(request):
-    pass
+    artistic_name = request.POST['artistic_name']
+    try:
+        artist = Artist.objects.get(artistic_name=artistic_name)
+    except Artist.DoesNotExist:
+        return HttpResponseNotFound()
+    artist.delete()
+    return redirect('artists')
